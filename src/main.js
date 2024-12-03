fd3a034cb0323dfaf0fb19478cd9248608db8bf3
Andrey Isenko <aisenko87@gmail.com>
2024-12-03 20:19:07 +0200

    add simpl

--- src/main.js
+++ src/main.js
@@ -9,13 +9,18 @@ const btN = document.querySelector('.but-sub');
 const API_KEY = '47413156-c8c9abea8f6d88937b7892740';
 let qData;
 let promData;
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+
 
 forM.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
   qData = event.currentTarget.elements.photo.value.trim();
-  console.log(event.currentTarget.elements.photo.value);
+//   console.log(event.currentTarget.elements.photo.value);
   searchData(`${qData}`);
 }
 
@@ -30,53 +35,53 @@ function searchData(datA = '') {
     return;
   }
   const params = new URLSearchParams({
-    key: API_KEY,
-
-    q: `${qData}`,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-
-  fetch(`https://pixabay.com/api/?${params}`)
+      key: API_KEY,
+      
+      q: `${qData}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+    });
+    
+    fetch(`https://pixabay.com/api/?${params}`)
     .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        return response.json();
     })
     .then(data => {
-      promData = data.hits.length;
-      console.log(data.hits);
-      console.log(promData);
-      //   gallerY.innerHTML = createMarkup(data.hits);
-      gallerY.insertAdjacentHTML('afterbegin', createMarkup(data.hits));
+        promData = data.hits;
+        console.log(data.hits.length);
+        console.log(promData);
+        //   gallerY.innerHTML = createMarkup(data.hits);
+        gallerY.insertAdjacentHTML('afterbegin', createMarkup(data.hits))
+        lightbox.refresh();
     })
     .catch(error => {
-      console.log(error);
+        console.log(error);
     });
-
-  function createMarkup(arr) {
-    return arr
-      .map(
-        ({
-          id,
-          webformatURL,
-          largeImageURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => `<li data-id="${id}" class="gallery-item">
-    <a href=${largeImageURL} class="gallery-link" ><img src=${webformatURL} class="gallery-image" alt=${tags}></a> </li>`
-      )
-      .join('');
-  }
+    
+    function createMarkup(arr) {
+        return arr
+        .map(
+            ({
+                id,
+                webformatURL,
+                largeImageURL,
+                tags,
+                likes,
+                views,
+                comments,
+                downloads,
+            }) => `<li data-id="${id}" class="gallery-item">
+            <a href=${largeImageURL} class="gallery-link" ><img src=${webformatURL} class="gallery-image" alt=${tags}></a> </li>`
+        )
+        .join('');
+    }
 }
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-  captionsData: 'alt',
-});
-// console.log(lightbox);
+
+console.log(lightbox);
+
+
